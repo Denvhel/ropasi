27b915b29cd522d1bd7372f50e52ea456388632a
Denvhel <dennisvanhelvoort@air-van-dennis.home>
2022-09-15 23:38:21 +0200

    work in progress

---
+++ ropasi.js
@@ -0,0 +1,11 @@
+function getComputerChoice(computerchoice) {
+    let choices = ['rock', 'paper', 'scissor'];
+    let choice = choices(Math.floor(Math.random() * choices.length));
+    return choice;
+}
+
+console.log(choice);
+
+
+
+
